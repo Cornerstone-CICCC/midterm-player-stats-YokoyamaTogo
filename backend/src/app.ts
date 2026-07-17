7c2c08d01f7b9d42d8368683c87815c76c74110c
kirakira <15catinthebox@gmail.com>
2026-07-16 17:18:30 -0700

    delete player nedpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import "dotenv/config"
 import cors from "cors"
 import express from "express"
-import { playerRouter } from "./routes/playerRoutes.js"
 import { performanceRouter } from "./routes/performanceRoutes.js"
 import { rankingRouter } from "./routes/rankingRoutes.js"
 
@@ -14,6 +13,5 @@ app.use(
 )
 app.use(express.json())
 
-app.use("/api/players", playerRouter)
 app.use("/api/performances", performanceRouter)
 app.use("/api/rankings", rankingRouter)
--- backend/src/routes/playerRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-import { getPlayerById, getPlayers } from "../controllers/playerController.js"
-
-export const playerRouter = Router()
-
-playerRouter.get("/", getPlayers)
-playerRouter.get("/:playerId", getPlayerById)
